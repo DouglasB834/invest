94e17abdd26a5bd3477405ada88da0304b882d57
DouglasB <douglas_borges09@hmail.com>
2024-11-08 09:48:57 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
   return (
-    <div>
-      <h1 className="flex items-start justify-between">chama nenenm</h1>
+    <div className="flex w-full flex-col items-center gap-4">
+      <h1>chama nenenm</h1>
+      <div>
+        <Button> criando projecto...</Button>
+      </div>
     </div>
   );
 };
